66d56130f3d19031a56c11733b45c5c8e5ff5ca1
VeroIrala <vero.be686@gmail.com>
2025-02-20 20:20:23 -0300

    modified:   Resources/SampleStyle.css 	modified:   proye_dos.html 	modified:   proye_dos.js

--- proye_dos.js
+++ proye_dos.js
@@ -1,9 +1,28 @@
+let balas = 10;
+
+const contadorElemento = document.getElementById ("contador");
+const audio = document.getElementById('audio');
+
+function actualizarContador() {
+    contadorElemento.textContent = `Balas restantes: ${balas.toString().padStart(2, '0')}`; 
+}
+
+
 document.querySelector(".target-circle").addEventListener("click", function(event) {
      
-    if (event.click);
-    const audio = document.getElementById('audio');
+    if (balas > 0) {
+
     audio.play();
+
+    balas--;
+    actualizarContador();
+
+    document.getElementById("mensaje").textContent = "";
+    } else {
+        document.getElementById("mensaje").textContent = "SIN MUNICION"
+    }
 });
 
+actualizarContador();
 
 
