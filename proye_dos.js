6303f02d4ef2f4fa833f5b60d480be8a9d846634
VeroIrala <vero.be686@gmail.com>
2025-02-20 18:25:35 -0300

    new file:   Resources/SampleStyle.css 	new file:   pistol-shot-233473.mp3 	new file:   proye_dos.html 	new file:   proye_dos.js 	new file:   res_ev.jpg

---
+++ proye_dos.js
@@ -0,0 +1,6 @@
+document.querySelector(".target-circle").addEventListener("click", function(event) {
+     
+    if (event.click);
+    const audio = document.getElementById('audio');
+    audio.play();
+});
