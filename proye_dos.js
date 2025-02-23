bd7c9858993c2fa81c62be7b93fcbd35c74a2c5b
VeroIrala <vero.be686@gmail.com>
2025-02-22 21:12:41 -0300

    TERMINADO CREO

--- proye_dos.js
+++ proye_dos.js
@@ -1,12 +1,24 @@
 let balas = 10;
+const max_balas = 10;  
 
-const contadorElemento = document.getElementById ("contador");
+const contadorElemento = document.getElementById ("contador").querySelector(".numero");
 const audio = document.getElementById('audio');
+const recarga = document.getElementById('recarga');
 
 function actualizarContador() {
-    contadorElemento.textContent = `Balas restantes: ${balas.toString().padStart(2, '0')}`; 
+    contadorElemento.textContent = balas.toString().padStart(2, '0'); 
 }
 
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'a' || event.key === 'A') { // Verifica si la tecla "a" es presionada
+        if (balas < max_balas) {
+            balas = max_balas; // Recargar las balas al máximo
+            actualizarContador();
+            recarga.play(); /* Se reproduce el audio de recarga*/
+            document.getElementById("mensaje").textContent = ""; // Limpiar mensaje si se recargaron
+        }
+    }
+});
 
 document.querySelector(".target-circle").addEventListener("click", function(event) {
      
@@ -20,10 +32,12 @@ document.querySelector(".target-circle").addEventListener("click", function(even
 
     document.getElementById("mensaje").textContent = "";
     } else {
-        document.getElementById("mensaje").textContent = "SIN MUNICION"
+        document.getElementById("mensaje").textContent = "¡SIN MUNICION!"
     }
 });
 
 actualizarContador();
+
+
 
 
